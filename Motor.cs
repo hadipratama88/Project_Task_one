a2b7697e752b7e49d3e9cf9fcd168496776caa26
Hadi Pratama <hdi_88@hotmail.com>
2013-05-27 23:07:25 +0700

    Project One
    Project One tentang class

---
+++ Motor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_Task_One
+{
+    class Motor
+    {
+        // membuat variabel/field untuk class Motor
+        public string NoMotor;
+        public string Merk;
+        public int Kecepatan;
+
+        // membuat method untuk menampilkan object
+        public void tampil()
+        {
+            Console.WriteLine("Motor: " + Merk + " Mempunyai Kecepatan " + Kecepatan + " Km/Jam, Nomor : " + NoMotor);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_Task_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat instan class/object
+            Motor mtr = new Motor();
+
+            // memasukan data instan class/object
+            mtr.Merk = "Honda";
+            mtr.Kecepatan = 100;
+            mtr.NoMotor = "D 1234 HP";
+
+            // memanggil method
+            mtr.tampil();
+            Console.ReadLine();
+        }
+    }
+}
